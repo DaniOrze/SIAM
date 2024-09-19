d91f5eafaf3bbd0d842a1c9e152362eba49d2851
Daniele Orzechowski <daniorzechowski@gmail.com>
2024-09-19 19:06:37 -0300

    feat(medication-dispensation): criação parcial do design da tela de medicação

--- siam-frontend/src/app/pages/medication-dispensation/medication-dispensation.component.ts
+++ siam-frontend/src/app/pages/medication-dispensation/medication-dispensation.component.ts
@@ -1,13 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-medication-dispensation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    NzLayoutModule,
+    NzGridModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    CommonModule,
+    NzSelectModule,
+    FormsModule],
   templateUrl: './medication-dispensation.component.html',
   styleUrl: './medication-dispensation.component.css'
 })
 export class MedicationDispensationComponent {
+
+  frequencias: any[] = [{ administrationTime: '', frequency: '' }];
+
+  adicionarFrequencia() {
+    this.frequencias.push({ administrationTime: '', frequency: '' });
+  }
+
+  removerFrequencia(index: number) {
+    this.frequencias.splice(index, 1);
+  }
 
 }
