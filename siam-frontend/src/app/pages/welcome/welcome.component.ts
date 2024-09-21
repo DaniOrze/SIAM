5e154fd965ca1fa262770e026636e41a3282cc66
Daniele Orzechowski <daniorzechowski@gmail.com>
2024-09-21 17:05:16 -0300

    feat(SCRUM-39): criação da tela inicial

--- siam-frontend/src/app/pages/welcome/welcome.component.ts
+++ siam-frontend/src/app/pages/welcome/welcome.component.ts
@@ -1,17 +1,71 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzCardModule } from 'ng-zorro-antd/card';
+
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxEchartsModule, NzGridModule, NzCardModule],
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrl: './welcome.component.css'
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent {
 
-  constructor() { }
+  medicationChartOptions: any;
+  adherenceChartOptions: any;
 
-  ngOnInit() { }
+  constructor() {
+    this.medicationChartOptions = {
+      title: {
+        text: 'Dispensação de Medicamentos'
+      },
+      tooltip: {},
+      xAxis: {
+        type: 'category',
+        data: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [5, 20, 36, 10, 10, 20, 30],
+          type: 'bar',
+          color: '#4CAF50'
+        }
+      ]
+    };
 
+    this.adherenceChartOptions = {
+      title: {
+        text: 'Adesão ao Tratamento'
+      },
+      tooltip: {
+        trigger: 'axis'
+      },
+      legend: {
+        data: ['Adesão']
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'Adesão',
+          type: 'line',
+          smooth: true,
+          data: [85, 88, 90, 75, 95],
+          areaStyle: {}
+        }
+      ],
+      color: ['#FF5722']
+    };
+  }
 }
