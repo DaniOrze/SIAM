5195a9241b2fd68aba21e1c611e96ed4fff4245e
Daniele Orzechowski <daniorzechowski@gmail.com>
2024-11-14 12:19:15 +0700

    feat(SCRUM-83): teste parcial do email

--- siam-backend/src/controllers/adherence.controller.ts
+++ siam-backend/src/controllers/adherence.controller.ts
@@ -23,6 +23,23 @@ export const registerDose = async (req: Request, res: Response) => {
     console.error("Erro ao registrar dose:", error);
     res.status(500).json({ error: "Erro ao registrar dose." });
   }
+
+  if(!taken) {
+    const formData = require('form-data');
+    const Mailgun = require('mailgun.js');
+    const mailgun = new Mailgun(formData);
+    const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || 'a240c7412dc7951b3dc30d4c943b7890-79295dd0-af0a683f'});
+  
+  mg.messages.create('sandbox-123.mailgun.org', {
+      from: "Excited User <mailgun@sandbox-123.mailgun.org>",
+      to: ["daniorzechowski@gmail.com"],
+      subject: "Hello",
+      text: "Testing some Mailgun awesomness!",
+      html: "<h1>Testing some Mailgun awesomness!</h1>"
+    })
+    .then((msg: any) => console.log(msg))
+    .catch((err: any) => console.error(err));
+  }
 };
 
 export const getAdherenceData = async (req: Request, res: Response) => {
--- siam-frontend/src/app/interceptor/auth.interceptor.ts
+++ siam-frontend/src/app/interceptor/auth.interceptor.ts
@@ -25,7 +25,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
     return next.handle(authReq).pipe(
       catchError((error: HttpErrorResponse) => {
-        if (error.status === 401) {
+        if (error.status === 401 || error.status == 403) {
           this.router.navigate(['/login']);
         }
         return throwError(error);
